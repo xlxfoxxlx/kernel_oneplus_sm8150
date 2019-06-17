01ab7f89c11ae8739b285be540b2a7f591b4fb5a
HolyAngel <slverwolf@gmail.com>
2019-06-17 14:42:50 -0500

    AndroidKernel.mk: Add guards for op7/7p

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),oneplus7pro))
 #Android makefile to build kernel as a part of Android Build
 PERL		= perl
 
@@ -199,5 +200,6 @@ kernelconfig: $(KERNEL_OUT) $(KERNEL_CONFIG)
 	     $(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(BUILD_ROOT_LOC)$(KERNEL_OUT) $(KERNEL_MAKE_ENV) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) $(real_cc) savedefconfig
 	cp $(KERNEL_OUT)/defconfig $(TARGET_KERNEL_SOURCE)/arch/$(KERNEL_ARCH)/configs/$(KERNEL_DEFCONFIG)
 
+endif
 endif
 endif
